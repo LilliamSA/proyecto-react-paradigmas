58357efcd72f014b54952443290b3ca7ffc7edd2
Lilliam Solis <lilliam.solis.avila@est.una.ac.cr>
2022-10-22 17:36:55 -0600

    validaciones de materiaUpdate

--- src/components/Materia/MateriaUpdate.js
+++ src/components/Materia/MateriaUpdate.js
@@ -12,13 +12,74 @@ const MateriaUpdate = () => {
     const [selectedOption, setSelectedOption] = useState(null);
     const [materia, setMateria] = useState({});
     const [periodos, setPeriodos] = useState([]);
-    const [submitted, setSubmitted] = useState(false);//submitted es el nombre de la variable y setSubmitted es el nombre de la funcion
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [cupos, setCupos] = useState("");
-    const [descripcion, setDescripcion] = useState(""); //que se actualice el valor de la identificacion
-    const [idMateria, setIdMateria] = useState(""); //que se actualice el valor del idPersona
-    const [idPeriodo, setIdPeriodo] = useState(""); //que se actualice el valor del idPersona
+    const [descripcion, setDescripcion] = useState(""); 
+    const [idMateria, setIdMateria] = useState(""); 
+    const [idPeriodo, setIdPeriodo] = useState(""); 
+    const [success, setSuccess] = useState(false);
+    const [err, setErr] = useState(false);
+    const [input, setInput] = useState(false);
+    const [errors, setErrors] = useState(false);
+
+
+       //validaciones
+       const validar = () => {
+        let regexCupos = /^[0-9]+$/;
+        let regexDescripcion = /^[a-zA-Z ]+$/;
+
+        if (descripcion === "" || cupos === "") {
+            return false;
+        } else if (!regexCupos.test(cupos)) {
+            return false;
+        } else if (!regexDescripcion.test(descripcion)) {
+            return false;
+        }else if (idPeriodo === "") {
+            return false;
+        }else if (selectedOption === null) {
+            return false;
+        }
+
+        return true;
+    };
+
+    const validarForm = () => {
+        //que acepte letras, numeros y espacios en blanco
+        let regexCupos = /^[0-9]+$/;
+        let regexDescripcion = /^[a-zA-Z ]+$/;
+
+        let errors = {};
+
+        if (descripcion === "") {
+            errors.descripcion = "La descripcion es requerida";
+        } else if (!regexDescripcion.test(descripcion)) {
+            errors.descripcion = "La descripcion solo puede contener letras";
+        }
+
+        if (cupos === "") {
+            errors.cupos = "Los cupos son requeridos";
+        } else if (!regexCupos.test(cupos)) {
+            errors.cupos = "Los cupos solo pueden contener numeros";
+        }
+
+        if (idPeriodo === "") {
+            errors.idPeriodo = "El periodo es requerido";
+        }else if (selectedOption === null) {
+            errors.idPeriodo = "El periodo es requerido";
+
+        }
+
+        return errors;
+
+    };
+
+    const reset = () => {
+        setSuccess(false);
+        setErr(false);
+        setInput(false);
+        setErrors(false);
+      };
 
     useEffect(() => {
         fetch(URL)
@@ -40,28 +101,26 @@ const MateriaUpdate = () => {
 
 
     const updateMateria = () => {
+        reset();
         var data = {
             id: idMateria,
             cupos: cupos,
             descripcion: descripcion,
             idPeriodo: idPeriodo.id,
         };
-        MateriaDataService.update(id, data)
-            .then(response => {
-                setMateria({
-                    id: response.data.id,
-                    cupos: response.data.cupos,
-                    descripcion: response.data.descripcion,
-                    idPeriodo: response.data.idPeriodo,
-
+        if (validar()) {
+            MateriaDataService.update(id, data)
+                .then(response => {
+                    setSuccess(true); 
+                })
+                .catch(e => {
+                    setErr(true);
                 });
-                setSubmitted(true);
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    }
+        } else {
+            setInput(true);
+        }
+    };
+
     const home = () => {
         window.location.href = "/materia/listar";
     }
@@ -81,23 +140,13 @@ const MateriaUpdate = () => {
             });
     };
 
-
     const handleChange = (selectedOption) => {
         setSelectedOption(selectedOption);
         setIdPeriodo({ id: selectedOption.value});
     }
-
     return (
+        <>
         <div className="submit-form">
-            {submitted ? (
-                <div>
-                    <h4>La materia se actualizo correctamente!</h4>
-                    <button className="btn btn-success" onClick={home}>
-                        Volver
-                    </button>
-                </div>
-            ) : (
-                <div>
                     <div className="form-group">
                         <label htmlFor="cupos">Cupos</label>
                         <input
@@ -108,7 +157,11 @@ const MateriaUpdate = () => {
                             value={cupos}
                             onChange={(e) => setCupos(e.target.value)}
                             name="cupos"
-                        />
+                            onBlur={() => setErrors(validarForm())}
+                            />
+                            {errors.cupos && (
+                              <p className="text-danger">{errors.cupos}</p>
+                            )}
                     </div>
                     <div className="form-group">
                         <label htmlFor="descripcion">Descripcion</label>
@@ -120,7 +173,11 @@ const MateriaUpdate = () => {
                             value={descripcion}
                             onChange={(e) => setDescripcion(e.target.value)}
                             name="descripcion"
-                        />
+                            onBlur={() => setErrors(validarForm())}
+                            />
+                            {errors.descripcion && (
+                              <p className="text-danger">{errors.descripcion}</p>
+                            )} 
                     </div>
                     <div className="form-group">
                         <label htmlFor="idperiodo">Periodo Actual</label>
@@ -150,15 +207,38 @@ const MateriaUpdate = () => {
                                 value: periodo.id,
                                 label: periodo.descripcion,
                             }))}
-                        />
+                            onBlur={() => setErrors(validarForm())}
+                            />
+                            {errors.idPeriodo && (
+                                <p className="text-danger">{errors.idPeriodo}</p>
+                            )}
                     </div>
                     <button onClick={updateMateria} className="btn btn-success">
                         Actualizar
                     </button>
                 </div>
-            )}
-        </div>
-    );
-};
+                  {success && (
+                    <div className="alert alert-success" role="alert">
+                      Materia actualizada con éxito!
+                      <br />
+                      <button onClick={home} className="btn btn-warning">
+                        Volver
+                      </button>
+                    </div>
+                  )}
+                  {err && (
+                    <div className="alert alert-danger" role="alert">
+                      Error al actualizar la materia!
+                    </div>
+                  )}
+                  {input && (
+                    <div className="alert alert-danger" role="alert">
+                      Por favor llene todos los campos o revise sus datos
+                    </div>
+                  )}
+                </>
+              );
+            };
+            
 
 export default MateriaUpdate;
