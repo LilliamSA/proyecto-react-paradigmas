265e6fb9b0f6088dfccfb28c0db76d91c18db25a
Victor Segura <seguravictor57@gmail.com>
2022-10-23 20:54:49 -0600

    estilos en PeriodoUpdate

--- src/components/Periodo/PeriodoUpdate.js
+++ src/components/Periodo/PeriodoUpdate.js
@@ -26,7 +26,7 @@ const PeriodoUpdate = () => {
       errors.descripcion = "La descripcion es requerida";
     } else if (!regexDescripcion.test(descripcion)) {
       errors.descripcion = "La descripcion solo puede contener letras y numeros";
-    } 
+    }
 
     return errors;
   };
@@ -89,60 +89,77 @@ const PeriodoUpdate = () => {
   };
 
   return (
-    <>
-      <div className="submit-form">
-        <div className="form-group">
-          <label htmlFor="identificacion">Identificacion</label>
-          <input
-            type="text"
-            className="form-control"
-            id="identificacion"
-            required
-            disabled
-            value={id}
-            name="identificacion"
-          />
+
+
+
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Formulario para actualizar un periodo</h1>
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="submit-form">
+            <div className="form-group">
+              <label htmlFor="identificacion">Identificacion</label>
+              <input
+                type="text"
+                className="form-control"
+                id="identificacion"
+                required
+                disabled
+                value={id}
+                name="identificacion"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="descripcion">Descripcion</label>
+              <input
+                type="text"
+                className="form-control"
+                id="descripcion"
+                required
+                value={descripcion}
+                onChange={(e) => setDescripcion(e.target.value)}
+                name="descripcion"
+                onBlur={() => setErrors(validarForm())}
+              />
+              {errors.descripcion && (
+                <p className="text-danger">{errors.descripcion}</p>
+              )}
+            </div>
+          </div>
+          <div className="row justify-content-center mt-4 mb-4">
+            <div className="col-md-6 text-center">
+              <button onClick={updatePeriodo} className="btn btn-success btn-lg">
+                Actualizar
+              </button>
+            </div>
+          </div>
+          <div className="row justify-content-center mt-4 mb-4">
+            <div className="col-md-6 text-center">
+              {success && (
+                <div className="alert alert-success" role="alert">
+                  Periodo actualizado correctamente
+                  <br />
+                  <button onClick={home} className="btn btn-warning">
+                    Volver
+                  </button>
+                </div>
+              )}
+              {err && (
+                <div className="alert alert-danger" role="alert">
+                  Error al actualizar el periodo
+                </div>
+              )}
+              {input && (
+                <div className="alert alert-danger" role="alert">
+                  Por favor ingrese todos los campos
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="descripcion">Descripcion</label>
-          <input
-            type="text"
-            className="form-control"
-            id="descripcion"
-            required
-            value={descripcion}
-            onChange={(e) => setDescripcion(e.target.value)}
-            name="descripcion"
-            onBlur={() => setErrors(validarForm())}
-            />
-            {errors.descripcion && (
-              <p className="text-danger">{errors.descripcion}</p>
-            )}
-        </div>
-        <button onClick={updatePeriodo} className="btn btn-success">
-          Actualizar
-        </button>
       </div>
-      {success && (
-        <div className="alert alert-success" role="alert">
-            Periodo actualizado correctamente
-          <br />
-          <button onClick={home} className="btn btn-warning">
-            Volver
-          </button>
-        </div>
-      )}
-      {err && (
-        <div className="alert alert-danger" role="alert">
-          Error al actualizar el periodo
-        </div>
-      )}
-      {input && (
-        <div className="alert alert-danger" role="alert">
-          Por favor ingrese todos los campos
-        </div>
-      )}
-    </>
+    </div>
+
   );
 };
 
