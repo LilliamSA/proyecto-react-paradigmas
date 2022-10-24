05acbdab8cca727ceba66d618085ebb8587c1483
Victor Segura <seguravictor57@gmail.com>
2022-10-23 19:50:25 -0600

    estilo en PersonaList

--- src/components/Persona/PersonaList.js
+++ src/components/Persona/PersonaList.js
@@ -1,5 +1,5 @@
 //hacer una tabla con la lista de personas
-import React from 'react';  
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PersonaDataService from '../../services/PersonaService';
 //crear una tabla
@@ -14,13 +14,13 @@ const PersonaList = (props) => {
     //obtener todas las personas
     const retrievePersonas = () => {
         PersonaDataService.getAll()
-        .then(response => {
-            setPersonas(response.data);
-            console.log(response.data);
-        })
-        .catch(e => {
-            console.log(e);
-        });
+            .then(response => {
+                setPersonas(response.data);
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
     };
     const setActivePersona = (persona, index) => {
         setCurrentPersona(persona);
@@ -30,64 +30,64 @@ const PersonaList = (props) => {
     const deletePersona = (id) => {
         if (window.confirm("¿Está seguro que desea eliminar esta persona?")) {
             PersonaDataService.deletePersona(id)
-            .then(response => {
-                console.log(response.data);
-                retrievePersonas();
-                alert("Persona eliminada con éxito");
-            })
-            .catch(e => {
-                console.log(e);
-                alert("No se pudo eliminar la persona, verifique que no tenga matriculas asociadas");
-            });
+                .then(response => {
+                    console.log(response.data);
+                    retrievePersonas();
+                    alert("Persona eliminada con éxito");
+                })
+                .catch(e => {
+                    console.log(e);
+                    alert("No se pudo eliminar la persona, verifique que no tenga matriculas asociadas");
+                });
         }
     };
-    
 
-    return(
-      //una tabla con los datos de la base de datos
-      <div className="container">
-        <div className="list row">
-            <div className="col-md-12">
-                <h4>Lista de personas</h4>
-                <table className="table table-striped table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Identificacion</th>
-                            <th>Nombre</th>
-                            <th>Editar</th>
-                            <th>Eliminar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {personas &&
-                            personas.map((persona, index) => (
-                                <tr
-                                    key={index}
-                                >
-                                    <td>{persona.id}</td>
-                                    <td>{persona.identificacion}</td>
-                                    <td>{persona.nombre}</td>
-                                    <td>
-                                    <Link to={"/persona/editar/" + persona.id} className="btn btn-success"> Editar </Link>
-                                    </td>
-                                    <td>
-                                        <button
-                                            className="btn btn-danger"
-                                            onClick={() => deletePersona(persona.id)}
-                                        >
-                                            Eliminar
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                    </tbody>
-                </table>
+
+    return (
+        //una tabla con los datos de la base de datos
+        <div className="container mt-5">
+            <h2 className="text-center mb-5">Lista de personas</h2>
+            <div className="list row justify-content-center">
+                <div className="col-md-12">
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Identificacion</th>
+                                <th>Nombre</th>
+                                <th>Editar</th>
+                                <th>Eliminar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {personas &&
+                                personas.map((persona, index) => (
+                                    <tr
+                                        key={index}
+                                    >
+                                        <td>{persona.id}</td>
+                                        <td>{persona.identificacion}</td>
+                                        <td>{persona.nombre}</td>
+                                        <td>
+                                            <Link to={"/persona/editar/" + persona.id} className="btn btn-success"> Editar </Link>
+                                        </td>
+                                        <td>
+                                            <button
+                                                className="btn btn-danger"
+                                                onClick={() => deletePersona(persona.id)}
+                                            >
+                                                Eliminar
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
-                            
+
 
 export default PersonaList;
