361c57fe1423bfdd2df34986dd89e3d75bd386bc
Victor Segura <seguravictor57@gmail.com>
2022-10-23 20:58:42 -0600

    estilos en PersonaUpdate

--- src/components/Persona/PersonaAdd.js
+++ src/components/Persona/PersonaAdd.js
@@ -95,7 +95,7 @@ const PersonaAdd = (props) => {
   return (
 
     <div className="container mt-5">
-      <h1 className="text-center mb-4">Formulario para agregar un periodo</h1>
+      <h1 className="text-center mb-4">Formulario para agregar una persona</h1>
       <div className="row justify-content-center">
         <div className="col-md-6">
           <div className="submit-form">
